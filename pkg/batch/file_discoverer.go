4dc9de1d3830440706f8879ffc7c1fce83742007
Dayang Shen <dayshen@microsoft.com>
2022-04-29 17:34:44 +0800

    Add unit tests for discoverers

--- pkg/batch/file_discoverer.go
+++ pkg/batch/file_discoverer.go
@@ -13,10 +13,12 @@ type FileBatchDiscoverer struct {
 
 func (d *FileBatchDiscoverer) Discover() ([]string, error) {
 	var file *os.File
+	var err error
+
 	if d.Path == "-" {
 		file = os.Stdin
 	} else {
-		file, err := os.Open(d.Path)
+		file, err = os.Open(d.Path)
 		if err != nil {
 			return nil, fmt.Errorf("Fail to open machines list file %s due to: %+v",
 				d.Path, err)
@@ -30,6 +32,11 @@ func (d *FileBatchDiscoverer) Discover() ([]string, error) {
 		line := string(scanner.Text())
 		line = strings.TrimSpace(line)
 		machines = append(machines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("Fail to read machines list file %s due to: %+v",
+			d.Path, err)
 	}
 
 	return machines, nil

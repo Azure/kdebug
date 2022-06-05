fb628909929d260bf5060103af91af135fed2caa
Jiahao Zhu <Jiahao.Zhu@microsoft.com>
2022-06-05 21:56:44 +0800

    detect crashing pods

--- pkg/checkers/kube/pod/pod_restart_reason_checker.go
+++ pkg/checkers/kube/pod/pod_restart_reason_checker.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"strings"
 	"text/tabwriter"
 	"time"
 
@@ -12,6 +13,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -24,6 +26,8 @@ import (
 	"k8s.io/kubectl/pkg/scheme"
 	"k8s.io/kubectl/pkg/util/qos"
 )
+
+const levelSpace = "  "
 
 type KubePodRestartReasonChecker struct {
 }
@@ -43,72 +47,39 @@ func (c *KubePodRestartReasonChecker) Check(ctx *base.CheckContext) ([]*base.Che
 		return nil, nil
 	}
 
-	name, namespace := ctx.Pod.Name, ctx.Pod.Namespace
-	pod, err := ctx.KubeClient.CoreV1().Pods(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+	pods, err := ctx.KubeClient.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		eventsInterface := ctx.KubeClient.CoreV1().Events(namespace)
-		selector := eventsInterface.GetFieldSelector(&name, &namespace, nil, nil)
-		initialOpts := metav1.ListOptions{
-			FieldSelector: selector.String(),
-			Limit:         util.DefaultChunkSize,
-		}
-		events := &corev1.EventList{}
-		err2 := runtimeresource.FollowContinue(&initialOpts,
-			func(options metav1.ListOptions) (runtime.Object, error) {
-				newList, err := eventsInterface.List(context.TODO(), options)
-				if err != nil {
-					return nil, runtimeresource.EnhanceListError(err, options, "events")
-				}
-				events.Items = append(events.Items, newList.Items...)
-				return newList, nil
-			})
-
-		if err2 != nil {
-			return []*base.CheckResult{
-				{
-					Checker: c.Name(),
-					Error:   err.Error(),
-				},
-			}, nil
-		}
-
-		if len(events.Items) == 0 {
-			return []*base.CheckResult{
-				{
-					Checker:     c.Name(),
-					Description: fmt.Sprintf("Nothing found for %s/%s", namespace, name),
-				},
-			}, nil
-		}
-
-		text, err3 := tabbedString(func(out io.Writer) error {
-			w := describe.NewPrefixWriter(out)
-			w.Write(describe.LEVEL_0, "Pod '%v': error '%v', but found events.\n", name, err)
-			describe.DescribeEvents(events, w)
-			return nil
-		})
-		if err3 != nil {
-			return nil, err3
-		}
-
-		return []*base.CheckResult{
-			{
-				Checker:     c.Name(),
-				Description: fmt.Sprintf("Information for %s/%s:\n%s", namespace, name, text),
-			},
-		}, nil
+		log.WithFields(log.Fields{"error": err}).Warn("Fail to list pods")
+		return nil, err
 	}
 
+	results := []*base.CheckResult{}
+	for _, pod := range pods.Items {
+		var crashing = false
+		for _, containerStatus := range pod.Status.ContainerStatuses {
+			if containerStatus.State.Waiting != nil && containerStatus.State.Waiting.Reason == "CrashLoopBackOff" {
+				crashing = true
+				break
+			}
+		}
+
+		if crashing {
+			result := c.checkPod(ctx, &pod)
+			if result != nil {
+				results = append(results, result)
+			}
+		}
+	}
+
+	return results, nil
+}
+
+func (c *KubePodRestartReasonChecker) checkPod(ctx *base.CheckContext, pod *v1.Pod) *base.CheckResult {
 	var events *corev1.EventList
 	ref, err := reference.GetReference(scheme.Scheme, pod)
 	if err != nil {
-		log.Errorf("Unable to construct reference to '%#v': %v", pod, err)
-		return []*base.CheckResult{
-			{
-				Checker: c.Name(),
-				Error:   err.Error(),
-			},
-		}, nil
+		log.WithFields(log.Fields{"pod": pod, "error": err}).Warn("Unable to construct reference")
+		return nil
 	}
 
 	ref.Kind = ""
@@ -116,13 +87,19 @@ func (c *KubePodRestartReasonChecker) Check(ctx *base.CheckContext) ([]*base.Che
 		ref.UID = types.UID(pod.Annotations[corev1.MirrorPodAnnotationKey])
 	}
 	events, _ = searchEvents(ctx.KubeClient.CoreV1(), ref, util.DefaultChunkSize)
-	text, err := describePodStatus(pod, events)
-	return []*base.CheckResult{
-		{
-			Checker:     c.Name(),
-			Description: fmt.Sprintf("Information for %s/%s:\n%s", namespace, name, text),
-		},
-	}, nil
+	text, _ := describePodStatus(pod, events)
+	logs := strings.Split(text, "\n")
+
+	for i := range logs {
+		logs[i] = levelSpace + logs[i]
+	}
+
+	return &base.CheckResult{
+		Checker:     c.Name(),
+		Error:       fmt.Sprintf("one or more containers of %s/%s are failing and restarting repeatedly.", pod.Namespace, pod.Name),
+		Description: fmt.Sprintf("%s/%s is not running well.", pod.Namespace, pod.Name),
+		Logs:        logs,
+	}
 }
 
 func describePodStatus(pod *corev1.Pod, events *corev1.EventList) (string, error) {
@@ -145,7 +122,7 @@ func describePodStatus(pod *corev1.Pod, events *corev1.EventList) (string, error
 		if len(pod.Status.Message) > 0 {
 			w.Write(describe.LEVEL_0, "Message:\t%s\n", pod.Status.Message)
 		}
-
+		describeContainers("Containers", pod.Spec.Containers, pod.Status.ContainerStatuses, describe.EnvValueRetriever(pod), w, "")
 		if len(pod.Status.Conditions) > 0 {
 			w.Write(describe.LEVEL_0, "Conditions:\n  Type\tStatus\n")
 			for _, c := range pod.Status.Conditions {
@@ -213,10 +190,87 @@ func searchEvents(client corev1client.EventsGetter, objOrRef runtime.Object, lim
 	return eventList, err
 }
 
+func describeContainers(label string, containers []corev1.Container, containerStatuses []corev1.ContainerStatus,
+	resolverFn describe.EnvVarResolverFunc, w describe.PrefixWriter, space string) {
+	statuses := map[string]corev1.ContainerStatus{}
+	for _, status := range containerStatuses {
+		statuses[status.Name] = status
+	}
+
+	for _, container := range containers {
+		status, ok := statuses[container.Name]
+		describeContainerBasicInfo(container, status, ok, space, w)
+		if ok {
+			describeContainerState(status, w)
+		}
+	}
+}
+
+func describeContainerBasicInfo(container corev1.Container, status corev1.ContainerStatus, ok bool, space string, w describe.PrefixWriter) {
+	nameIndent := ""
+	if len(space) > 0 {
+		nameIndent = " "
+	}
+	w.Write(describe.LEVEL_1, "%s%v:\n", nameIndent, container.Name)
+	if ok {
+		w.Write(describe.LEVEL_2, "Container ID:\t%s\n", status.ContainerID)
+	}
+	w.Write(describe.LEVEL_2, "Image:\t%s\n", container.Image)
+	if ok {
+		w.Write(describe.LEVEL_2, "Image ID:\t%s\n", status.ImageID)
+	}
+}
+
+func describeContainerState(status corev1.ContainerStatus, w describe.PrefixWriter) {
+	describeStatus("State", status.State, w)
+	if status.LastTerminationState.Terminated != nil {
+		describeStatus("Last State", status.LastTerminationState, w)
+	}
+	w.Write(describe.LEVEL_2, "Ready:\t%v\n", printBool(status.Ready))
+	w.Write(describe.LEVEL_2, "Restart Count:\t%d\n", status.RestartCount)
+}
+
+func describeStatus(stateName string, state corev1.ContainerState, w describe.PrefixWriter) {
+	switch {
+	case state.Running != nil:
+		w.Write(describe.LEVEL_2, "%s:\tRunning\n", stateName)
+		w.Write(describe.LEVEL_3, "Started:\t%v\n", state.Running.StartedAt.Time.Format(time.RFC1123Z))
+	case state.Waiting != nil:
+		w.Write(describe.LEVEL_2, "%s:\tWaiting\n", stateName)
+		if state.Waiting.Reason != "" {
+			w.Write(describe.LEVEL_3, "Reason:\t%s\n", state.Waiting.Reason)
+		}
+	case state.Terminated != nil:
+		w.Write(describe.LEVEL_2, "%s:\tTerminated\n", stateName)
+		if state.Terminated.Reason != "" {
+			w.Write(describe.LEVEL_3, "Reason:\t%s\n", state.Terminated.Reason)
+		}
+		if state.Terminated.Message != "" {
+			w.Write(describe.LEVEL_3, "Message:\t%s\n", state.Terminated.Message)
+		}
+		w.Write(describe.LEVEL_3, "Exit Code:\t%d\n", state.Terminated.ExitCode)
+		if state.Terminated.Signal > 0 {
+			w.Write(describe.LEVEL_3, "Signal:\t%d\n", state.Terminated.Signal)
+		}
+		w.Write(describe.LEVEL_3, "Started:\t%s\n", state.Terminated.StartedAt.Time.Format(time.RFC1123Z))
+		w.Write(describe.LEVEL_3, "Finished:\t%s\n", state.Terminated.FinishedAt.Time.Format(time.RFC1123Z))
+	default:
+		w.Write(describe.LEVEL_2, "%s:\tWaiting\n", stateName)
+	}
+}
+
 func translateTimestampSince(timestamp metav1.Time) string {
 	if timestamp.IsZero() {
 		return "<unknown>"
 	}
 
 	return duration.HumanDuration(time.Since(timestamp.Time))
+}
+
+func printBool(value bool) string {
+	if value {
+		return "True"
+	}
+
+	return "False"
 }

496fd6b2aa09c5caecbb436b3a47db68904fb061
Dayang Shen <dayshen@microsoft.com>
2022-07-14 18:18:50 +0800

    Fix http checker

--- pkg/checkers/http/http.go
+++ pkg/checkers/http/http.go
@@ -3,7 +3,6 @@ package http
 import (
 	"fmt"
 	"net/http"
-	"net/url"
 	"time"
 
 	"github.com/Azure/kdebug/pkg/base"
@@ -13,31 +12,21 @@ import (
 var (
 	GoogleTarget = HttpTarget{
 		Name: "Google HTTP endpoint",
-		Request: &http.Request{
-			URL: &url.URL{
-				Scheme: "https",
-				Host:   "google.com",
-			},
-		},
+		URL:  "https://google.com",
 	}
 	AzureIMDSTarget = HttpTarget{
 		Name: "Azure IMDS HTTP endpoint",
-		Request: &http.Request{
-			URL: &url.URL{
-				Scheme: "https",
-				Host:   "169.254.169.254",
-				Path:   "metadata/versions",
-			},
-			Header: map[string][]string{
-				"Metadata": {"true"},
-			},
+		URL:  "http://169.254.169.254/metadata/versions",
+		Header: http.Header{
+			"Metadata": {"true"},
 		},
 	}
 )
 
 type HttpTarget struct {
-	Name    string
-	Request *http.Request
+	Name   string
+	URL    string
+	Header http.Header
 }
 
 type HttpChecker struct {
@@ -67,19 +56,28 @@ func (c *HttpChecker) Check(ctx *base.CheckContext) ([]*base.CheckResult, error)
 	targets := getCheckTargets(ctx.Environment)
 	var result *base.CheckResult
 	for _, httpTarget := range targets {
-		response, err := c.Client.Do(httpTarget.Request)
+		request, err := http.NewRequest("GET", httpTarget.URL, nil)
+		if err != nil {
+			return nil, fmt.Errorf("Fail to create request for target %s: %+v",
+				httpTarget.Name, err)
+		}
+		request.Header = httpTarget.Header
+
+		response, err := c.Client.Do(request)
 		if err != nil {
 			result = &base.CheckResult{
-				Checker:     c.Name(),
-				Error:       fmt.Sprintf("Fail to invoke HTTP GET method on URL %s.", httpTarget.Request.RequestURI),
+				Checker: c.Name(),
+				Error: fmt.Sprintf("Fail to invoke HTTP GET method on URL %s.",
+					httpTarget.URL),
 				Description: err.Error(),
 				//todo: Recommendations and help links
 			}
 		} else {
 			defer response.Body.Close()
 			result = &base.CheckResult{
-				Checker:     c.Name(),
-				Description: fmt.Sprintf("Successfully invoke HTTP GET on URL %s , response status code is %s.", httpTarget.Request.RequestURI, response.Status),
+				Checker: c.Name(),
+				Description: fmt.Sprintf("Successfully invoke HTTP GET on URL %s , response status code is %s.",
+					httpTarget.URL, response.Status),
 			}
 		}
 		results = append(results, result)

a22d842b805548303eea0c98d2253b0d3f2bf506
Zitong Yang <zityang@microsoft.com>
2022-09-21 14:01:11 +0800

    Find out the file system of home dir for user

--- pkg/checkers/diskreadonly/disk_readonly.go
+++ pkg/checkers/diskreadonly/disk_readonly.go
@@ -1,16 +1,26 @@
 package diskreadonly
 
 import (
-	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/Azure/kdebug/pkg/base"
 )
 
 const (
-	HomeDirReadonlyRecommendation = "The disk enters read-only state due to underlying data integrity issues. Find out which disk your home dir is mounted on via 'df' command. Try to use 'fsck' command to fix the disk and then reboot the vm."
+	Reason                = "The filesystem mignt enter read-only state due to underlying data integrity issues."
+	GeneralRecommendation = "Find out which filesystem your home dir is mounted on via 'df' command. Try to use 'fsck' command to fix the filesystem and then reboot the vm."
 )
+
+var helpLink = []string{
+	"https://askubuntu.com/a/197468",
+	"https://man7.org/linux/man-pages/man8/fsck.8.html",
+}
 
 type DiskReadOnlyChecker struct {
 }
@@ -24,18 +34,35 @@ func (c *DiskReadOnlyChecker) Name() string {
 }
 
 func (c *DiskReadOnlyChecker) Check(ctx *base.CheckContext) ([]*base.CheckResult, error) {
+	if !strings.HasPrefix(runtime.GOOS, "linux") {
+		// This checker is only valid on Linux.
+		log.Infof("Skip %s checker as it not applicable on %s", c.Name(), runtime.GOOS)
+		return []*base.CheckResult{}, nil
+	}
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return nil, errors.New("Fail to get user home directory and check if it's read-only")
+		return nil, fmt.Errorf("Fail to get user home dir. %w", err)
 	}
 	f, err := os.CreateTemp(homeDir, "testReadOnlyFile")
 	var result *base.CheckResult
 	if err != nil {
-		result = &base.CheckResult{
-			Checker:         c.Name(),
-			Error:           fmt.Sprintf("Fail to create a temp file in %s", homeDir),
-			Description:     err.Error(),
-			Recommendations: []string{HomeDirReadonlyRecommendation},
+		var recommendation string
+		if strings.Contains(strings.ToLower(err.Error()), "read-only") {
+			mountSrc, mountTarget, findMntErr := getMountSrcAndTarget(homeDir)
+			if findMntErr != nil {
+				recommendation = fmt.Sprintf("%s%s", Reason, GeneralRecommendation)
+			} else {
+				recommendation = fmt.Sprintf("%s Try to use 'fsck' command to fix the %s mounted on %s and then reboot the vm.", Reason, mountSrc, mountTarget)
+			}
+			result = &base.CheckResult{
+				Checker:         c.Name(),
+				Error:           fmt.Sprintf("Fail to create a temp file in %s", homeDir),
+				Description:     err.Error(),
+				Recommendations: []string{recommendation},
+				HelpLinks:       []string{},
+			}
+		} else {
+			return nil, fmt.Errorf("Fail to create a temp file in %s due to unexpected error: %w", homeDir, err)
 		}
 	} else {
 		defer os.Remove(f.Name())
@@ -45,4 +72,18 @@ func (c *DiskReadOnlyChecker) Check(ctx *base.CheckContext) ([]*base.CheckResult
 		}
 	}
 	return []*base.CheckResult{result}, nil
+}
+
+func getMountSrcAndTarget(homeDir string) (string, string, error) {
+	findMntCmd := exec.Command("findmnt", "--target", homeDir, "--output", "SOURCE,TARGET", "--noheadings")
+	mountDescription, err := findMntCmd.Output()
+	if err != nil {
+		err = fmt.Errorf("Fail to find the filesystem of %s with command '%s': %w", homeDir, findMntCmd.String(), err)
+		log.Warn(err)
+		return "", "", err
+	} else {
+		mountDescriptions := strings.Split(strings.TrimSuffix(string(mountDescription), "\n"), " ")
+		// mount source, mount target, error
+		return mountDescriptions[0], mountDescriptions[1], nil
+	}
 }
